3402ad0732a9c7a799078227665cc12fe137f120
t-lamp11 <t-lamp11@users.noreply.github.com>
2016-04-01 21:01:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var HTTPS = require('https'),
 
 function respond() {
     var request = JSON.parse(this.req.chunks[0]),
-        botRegex = Amanda/i;
+        botRegex = require("Amanda");
 
     if(request.text && botRegex.test(request.text)) {
         this.res.writeHead(200);
